cff729fdfc098bbe4873e60f4d1b53422019d90e
anian818 <110804984+anian818@users.noreply.github.com>
2023-09-05 20:01:24 +0800

    Update style.js

--- themes/matery/style.js
+++ themes/matery/style.js
@@ -8,7 +8,7 @@ const Style = () => {
   return <style jsx global>{`
     // 底色
     body{
-        background-color: #f5f5f5
+        background-color: light blue
     }
     .dark body{
         background-color: light blue;
